67ed422a6eff42b4e767d0143939c12722925261
Musta1song <147011583+Musta1song@users.noreply.github.com>
2024-02-12 00:22:14 -0500

    sareqwfrawr,älghotbkmtaempbgjaerwgkjERWGF+ẞPRWghoüae5

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import vuetify from './plugins/vuetify'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import router from './router'
 
 createApp(App).use(router).use(vuetify).mount('#app')
